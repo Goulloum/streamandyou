b5659406bdbc72ab48f8101af0b44df2e6f49223
mathieu.guillemin <mathieu.guillemin@iscio.com>
2022-11-14 22:11:03 +0100

    oupsi j'avais oublier de faire le delete

---
+++ src/Service/IService.ts
@@ -0,0 +1,9 @@
+import { StreamerDTO } from "../Mapper/StreamerMapper";
+
+export interface IService {
+    add(raw: any): Promise<StreamerDTO>;
+    delete(id: number): Promise<Boolean>
+    findById(id: number): Promise<StreamerDTO>
+    findAll(): Promise<StreamerDTO[]>
+    update(raw: any): 
+}
---
+++ src/Service/StreamerService.ts
@@ -0,0 +1,3 @@
+import { IService } from "./IService";
+
+export class StreamerService implements IService {}
