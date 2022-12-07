4e836a6d825436c026ace840bc3bbf9114d1b0ac
Mathieu GUILLEMIN <mathieu.guillemin@sfl.com>
2022-12-07 10:41:48 +0100

    ça fait bcp

--- src/Mapper/CompanyMapper.ts
+++ src/Mapper/CompanyMapper.ts
@@ -1,11 +1,8 @@
-import { Announcement } from "../Model/Announcement";
 import { Company } from "../Model/Company";
-import { AnnouncementDTO, AnnouncementMapper } from "./AnnouncementMapper";
 
 export interface CompanyDTO {
     id: number;
     name: string;
-    announcements: AnnouncementDTO[];
 }
 
 export class CompanyMapper {
@@ -13,7 +10,6 @@ export class CompanyMapper {
         return {
             id: companyModel.id,
             name: companyModel.dataValues.name,
-            announcements: companyModel.dataValues.announcements?.map((announcement: Announcement) => AnnouncementMapper.toDTO(announcement)),
         };
     };
 }
